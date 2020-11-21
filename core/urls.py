75bbb3ddac4d6b131fc8c5d662e585b102d15a11
alaravena <55965716+alaravena@users.noreply.github.com>
2020-11-21 16:02:35 -0300

    impostor

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,6 @@
 from django.urls import path
-from core.views import index, compra, productos, galeriaAdmin, productosAdmin, promocionesinicioAdmin, promocionesproductosAdmin, categoriaAdmin, inicioAdmin, modificarProductoAdmin, eliminarProducto, eliminarCategoria, eliminarGaleria, eliminarPromocionInicio, eliminarPromocionProducto, registro
+from .views import home
 
 urlpatterns = [
-    path('', index,name="index"),
-    path('productos/', productos,name="productos"),
-    path('productos/compra/', compra,name="compra"),
-    path('galeriaAdmin/', galeriaAdmin, name="galeriaAdmin"),
-    path('productosAdmin/', productosAdmin,name="productosAdmin"),
-    path('categoriaAdmin/', categoriaAdmin,name="categoriaAdmin"),
-    path('promocionesinicioAdmin/', promocionesinicioAdmin,name="promocionesinicioAdmin"),
-    path('promocionesproductosAdmin/', promocionesproductosAdmin,name="promocionesproductosAdmin"),
-    path('inicioAdmin/', inicioAdmin, name="inicioAdmin"),
-    path('modificarProductoAdmin/<id>/', modificarProductoAdmin, name="modificarProductoAdmin"),
-    path('eliminarProducto/<id>/', eliminarProducto, name="eliminarProducto"),
-    path('eliminarCategoria/<id>/', eliminarCategoria, name="eliminarCategoria"),
-    path('eliminarGaleria/<id>/', eliminarGaleria, name="eliminarGaleria"),
-    path('eliminarPromocionInicio/<id>/', eliminarPromocionInicio, name="eliminarPromocionInicio"),
-    path('eliminarPromocionProducto/<id>/', eliminarPromocionProducto, name="eliminarPromocionProducto"),
-    path('registro/', registro, name="registro"),
+    path('', home, name="home"),
 ]
