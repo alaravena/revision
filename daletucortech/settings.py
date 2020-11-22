6738c80daf2268f94388649d42a1ef858e7ec1a6
alaravena <55965716+alaravena@users.noreply.github.com>
2020-11-22 09:51:11 -0300

    set

--- daletucortech/settings.py
+++ daletucortech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v-l(!86aszkz+r0qq7l%u&aptla+2=nov^3zv^5w4-rzi7jx_='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','daletucortech.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 LOGIN_REDIRECT_URL = '/'
 
